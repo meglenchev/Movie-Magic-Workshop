ae2c54b11b89aff8241517558223bac469a3b062
meglenchev <t.meglenchev@gmail.com>
2025-09-17 16:47:14 +0300

    Setup ExpressJS server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 
+// Setup Express Server
 const app = express();
+
+// Setup Handlebars
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
+
 app.set('view engine', 'hbs');
+
+app.listen(5000, () => console.log('Server is listening on http://localehost:5000...'))
